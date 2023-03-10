46b47d9b9d49c7ad19cf97b37d54a8874caf677b
Bernardo Chagas Araújo <bernardochagasaraujo@gmail.com>
2023-03-10 02:46:39 -0300

    setup theme

---
+++ origin-financial-web-app/src/theme.js
@@ -0,0 +1,25 @@
+import {createTheme} from '@mui/system'
+
+export const theme = createTheme({
+    palette: {
+        text: {
+            main: '#1E2A32',
+            secondary: '#4D6475',
+        },
+        primary: {
+            main: '#001C95',
+        },
+        info: {
+            main: '#708797',
+        },
+        success: {
+            'main': '#04C761',
+        },
+        warning: {
+            'main': '#FFC032',
+        },
+        error: {
+            'main': '#D32A36',
+        },
+    },
+})
