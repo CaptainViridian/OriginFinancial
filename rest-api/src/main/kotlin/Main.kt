4fe6412b60853718d5638300d5e2e2a540cc392a
Bernardo Chagas Araújo <bernardochagasaraujo@gmail.com>
2023-03-10 01:32:59 -0300

    Add new route 'evaluateFinancialHealth' to Server entry point

--- rest-api/src/main/kotlin/Main.kt
+++ rest-api/src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import http.controllers.FinancialHealthEvaluationController
 import io.javalin.Javalin
+import services.FinancialHealthEvaluationService
 
 fun main() {
+    val evaluationService = FinancialHealthEvaluationService()
+    val objectMapper = jacksonObjectMapper()
+    val controller = FinancialHealthEvaluationController(
+        evaluationService,
+        objectMapper
+    )
+
     Javalin.create()
         .get("/") { ctx -> ctx.result("Hello World") }
+        .post("/evaluateFinancialHealth", controller::post)
         .start(3000)
 }
