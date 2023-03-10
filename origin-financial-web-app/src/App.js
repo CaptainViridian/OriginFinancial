b556390aff428981e037399f667f9d58d5a606b3
Bernardo Chagas Araújo <bernardochagasaraujo@gmail.com>
2023-03-10 16:33:54 -0300

    adjust App background and component positions

--- origin-financial-web-app/src/App.js
+++ origin-financial-web-app/src/App.js
@@ -1,13 +1,27 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import '@fontsource/work-sans'
+import {styled} from '@mui/system'
+
+const AppRoot = styled('div')(({theme}) => ({
+    backgroundColor: theme.palette.background,
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+        justifyContent: 'space-between',
+    },
+}))
 
 function App() {
     return (
-        <div id="App">
+        <AppRoot>
             <Header />
+
             <Footer />
-        </div>
+        </AppRoot>
     )
 }
 
--- origin-financial-web-app/src/components/Header.jsx
+++ origin-financial-web-app/src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = styled('header')(({theme}) => ({
     display: 'flex',
     justifyContent: 'center',
     padding: theme.spacing(3),
+    backgroundColor: '#fff',
 }))
 
 export default function AppHeader() {
